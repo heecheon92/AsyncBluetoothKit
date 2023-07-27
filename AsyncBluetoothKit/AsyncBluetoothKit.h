491b14bc03cf3b14a5c7a62c89d0ad475624efdf
Heecheon Park <heecheon92@gmail.com>
2023-07-27 10:42:48 +0900

    adding base files

---
+++ AsyncBluetoothKit/AsyncBluetoothKit.h
@@ -0,0 +1,18 @@
+//
+//  AsyncBluetoothKit.h
+//  AsyncBluetoothKit
+//
+//  Created by Heecheon Park on 7/27/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AsyncBluetoothKit.
+FOUNDATION_EXPORT double AsyncBluetoothKitVersionNumber;
+
+//! Project version string for AsyncBluetoothKit.
+FOUNDATION_EXPORT const unsigned char AsyncBluetoothKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AsyncBluetoothKit/PublicHeader.h>
+
+
